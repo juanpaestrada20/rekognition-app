8a1db3c0fae25ea88b00744bfcf0b4300f8168f5
Juan Pablo Estrada <3599546500101@ingenieria.usac.edu.gt>
2021-03-04 13:33:49 -0600

    [ADD] uso de rekognition

--- app.py
+++ app.py
@@ -1,6 +1,41 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import boto3
+from botocore.client import Config
+import base64
+
+import json
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+with open('keys.json') as json_file:
+    data = json.load(json_file)
+
+
+def getRekognition():
+    rekognition = boto3.client(
+        'rekognition',
+        region_name=data['rekognition']['region'],
+        aws_access_key_id=data['rekognition']['accessKeyId'],
+        aws_secret_access_key=data['rekognition']['secretAccessKey'],
+    )
+    return rekognition
+
+
+rek = getRekognition()
+
+
+@app.route('/tarea3-201800709')
+def rekognition():
+    image = json.loads(request.data)['image']
+    bytesImage = base64.b64decode(image)
+    res = rek.detect_labels(
+        Image={'Bytes': bytesImage},
+        MaxLabels=15)
+    result = {
+        'status': 200,
+        'image': res
+    }
+    response = {
+        'result': result['image']['Labels']
+    }
+    return jsonify(response), result['status'], {'Content-Type': 'application/json'}
